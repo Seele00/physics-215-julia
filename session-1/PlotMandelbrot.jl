3a96a21dc4f8045c96ab54d7b6e1863aed708c76
Seele00 <gldizon@nip.upd.edu.ph>
2021-10-10 15:52:15 +0800

    drafted notebook up to KR5

--- session-1/PlotMandelbrot.jl
+++ session-1/PlotMandelbrot.jl
@@ -1,12 +1,34 @@
-function mandel(x, y)
-	c = x + y*im
+using Plots
+using ColorSchemes
+gr()
+
+function mandelbrot(c)
 	z = c
 	maxiter = 80
 	for n in 1:maxiter
 		if abs(z) > 2
-			return n-1
+			return (n-1)::Int
 		end
 		z = z^2 + c
 	end
-	return maxiter
+	return maxiter::Int
 end
+
+function plotMandelbrot(xrange::Tuple{Float64, Float64}, yrange::Tuple{Float64, Float64}; n::Int)
+	xaxis = collect(LinRange(xrange[1], xrange[2], n))
+	yaxis = collect(LinRange(yrange[1], yrange[2], n))
+	grid = zeros((n, n))
+	
+	for i in range(1, length = n)
+		for j in range(1, length = n)
+			x = xaxis[i]
+			y = yaxis[j]
+			iteration = mandelbrot(x + y*im)
+			grid[i,j] = iteration
+		end
+	end
+	p = heatmap(xaxis, yaxis, grid, c = :thermal, aspect_ratio = 1)
+	display(p)
+end
+
+plotMandelbrot(vrange::Tuple{Float64, Float64}; n::Int) = plotMandelbrot(vrange, vrange; n)
